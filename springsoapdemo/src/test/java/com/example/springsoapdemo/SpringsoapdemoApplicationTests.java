f04daaeefe62db37b864434a2f11b0569068c071
abdoulfataho <yamyaogofataho@gmail.com>
2024-09-16 15:34:40 -0400

    final

---
+++ springsoapdemo/src/test/java/com/example/springsoapdemo/SpringsoapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsoapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsoapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
